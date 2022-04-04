1dd17979386a1d48c1650d4eba168aa70572a4d2
Márcio Alves <alvesmarcio@hotmail.com>
2022-04-04 15:51:41 -0300

    Feature: home route redefined withou prefix

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask, Blueprint
 from app.routes.anime_routes import bp as anime_bp
 
 
-bp_v1 = Blueprint("v1", __name__, url_prefix="/v1")
+bp_home = Blueprint("home", __name__, url_prefix="")
 
 def init_app(app: Flask):
-    bp_v1.register_blueprint(anime_bp)
+    bp_home.register_blueprint(anime_bp)
     
-    app.register_blueprint(bp_v1)
+    app.register_blueprint(bp_home)
     
